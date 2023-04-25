18d8a8d2fe474b09fccbc8e2267291fbc5c16417
Fredy Gonzalez <fredylingling.fgg6@gmail.com>
2023-04-25 15:45:00 -0500

    en el formulario de contacto se le asigno un titulo, se agrego id al formulario,sse agrego el  atributo name para cada input y poder usar formData, y se reemplazo el input onClick por un button type submit

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const textoNosotros = document.querySelector(".texto-nosotros");
 const p = document.querySelector("p");
 const parrafoDigitado = document.querySelector("#nombreDigitado");
 //datos del formulario contacto.
+const formContacto = document.querySelector("#formContacto");
 const inputNombre = document.querySelector("#nombre");
 const inputAsunto = document.querySelector("#asunto");
 const inputEmail = document.querySelector("#email");
@@ -15,17 +16,30 @@ const inputMensaje = document.querySelector("#mensaje");
 //   inputTelefono.value,
 //   inputMensaje.value
 // );
-const inputEnviarFormulario = document.querySelector("#enviarFormulario");
+// const inputEnviarFormulario = document.querySelector("#enviarFormulario");
 //fin de variables de datos del formulario contacto.
 
 const parrafoNosotros = document.querySelector(".parrafo");
 
 // parrafoNosotros.innerText =
 //   "estoy usando .innerText para manipular este elemento desde js ";
-inputEnviarFormulario.addEventListener("click", function (event) {
-  event.preventDefault();
+
+formContacto.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log("funciona");
+  var datos = new FormData(formContacto);
+  console.log(datos);
+  console.log(datos.get("name"));
+  console.log(datos.get("asunto"));
+  console.log(datos.get("email"));
+  console.log(datos.get("tel"));
+  console.log(datos.get("mensaje"));
 });
-function btnOnClick() {
-  const Nombre = inputNombre.value;
-  parrafoDigitado.innerText = "Bienvenido: " + Nombre;
-}
+
+// inputEnviarFormulario.addEventListener("click", function (event) {
+//   event.preventDefault();
+// });
+// function btnOnClick() {
+//   const Nombre = inputNombre.value;
+//   parrafoDigitado.innerText = "Bienvenido: " + Nombre;
+// }
