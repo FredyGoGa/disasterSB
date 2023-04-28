cabf0b867f7363f2929a7217916de13ef5bdbc7d
Fredy Gonzalez <fredylingling.fgg6@gmail.com>
2023-04-28 11:09:43 -0500

    en el index se hicieron pruebas de imagenes, se comento codigo de prueba y en los estilos se elimino un selector nav que estaba ocasionando error en la posicion, elementos del footer organizados correctamente

--- script.js
+++ script.js
@@ -11,24 +11,36 @@ const inputTelefono = document.querySelector("#tel");
 const inputMensaje = document.querySelector("#mensaje");
 const inputEnviar = document.querySelector("#enviar");
 //fin de variables de datos del formulario contacto.
-const parrafoNosotros = document.querySelector(".parrafo");
+// const parrafoNosotros = document.querySelector(".parrafo");
 const inputPais = document.querySelector("#paises");
-const paisSeleccionado = inputPais.value;
+// const paisSeleccionado = inputPais.value;
+const logoUp = document.querySelector(".logoUP");
+
+const nombreSitio = document.querySelector(".inactive");
+if (logoUp) {
+  logoUp.addEventListener("click", toggleLogoUp);
+
+  function toggleLogoUp() {
+    nombreSitio.classList.toggle(".inactive");
+  }
+}
 
 // parrafoNosotros.innerText =
 //   "estoy usando .innerText para manipular este elemento desde js ";
+if (formContacto) {
+  formContacto.addEventListener("submit", function (evento) {
+    evento.preventDefault();
+    var datos = new FormData(formContacto);
+    console.log(datos);
+    console.log(datos.get("name"));
+    console.log(datos.get("asunto"));
+    console.log(datos.get("email"));
+    console.log(datos.get("tel"));
+    console.log(datos.get("mensaje"));
+    console.log(paisSeleccionado);
+  });
+}
 
-formContacto.addEventListener("submit", function (evento) {
-  evento.preventDefault();
-  var datos = new FormData(formContacto);
-  console.log(datos);
-  console.log(datos.get("name"));
-  console.log(datos.get("asunto"));
-  console.log(datos.get("email"));
-  console.log(datos.get("tel"));
-  console.log(datos.get("mensaje"));
-  console.log(paisSeleccionado);
-});
 // function enviarDatosFormulario(event) {
 //   event.preventDefault();
 //   const Nombre = inputNombre.value;
