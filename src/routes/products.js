62728b23d0266e632860c94ee62687f760fe1d08
Fredy Gonzalez Garcia <fredylingling.fgg6@gmail.com>
2023-07-07 11:11:15 -0500

    crud complete for route products

--- src/routes/products.js
+++ src/routes/products.js
@@ -12,7 +12,7 @@ router.post('/',(req, res) => {
     const {id, nombre, marca, descripción, precio, disponible} = req.body;
     if (id || nombre || marca || descripción || precio || disponible) {
         const id = products.length +1 ;
-        const newProduct = {...req.body, id};
+        const newProduct = {...req.body,id};
         products.push(newProduct);
         res.json(products);        
     } else {
@@ -20,13 +20,35 @@ router.post('/',(req, res) => {
     }
 });
 
-router.delete('/:id',(req, res) =>{
+router.delete('/:id',(req,res) =>{
     const {id} = req.params;
-    _.each(products,(product,i) =>{
+    _.each(products, (product,i) =>{
         if (product.id == id) {
             products.splice(i,1);
         } 
        });
        res.send(products);
+});
+
+router.put('/:id', (req,res) =>{
+    const {id} = req.params;
+    const { nombre, marca, descripción, precio, disponible } = req.body;
+    if (nombre && marca && descripción && precio && disponible) {
+        _.each(products, (product,i) =>{
+            if (product.id == id) {
+                product.nombre = nombre;
+                product.marca = marca;
+                product.descripción = descripción;
+                product.precio = precio;
+                product.disponible = disponible;
+            } 
+        });
+        res.json(products);
+    } else {
+        res.status(500).json({error: 'error'});
+    }
+    
 })
+
+
 module.exports = router;
